86138b3cd4f81639a9c19f157b90044cc1404a57
JPZ <jeanpaulzune@gmail.com>
2018-01-22 22:52:50 -0500

    Updated active book reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// State argmetn is not appliaction state, only the
+// state = null Defines the state to null if state is undefined
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+        return action.payload;
+  }
+  return state;
+}
