216f51bddb2eda24879117a224e1d9ce9bed194e
JPZ <jeanpaulzune@gmail.com>
2018-01-24 17:54:08 -0500

    Added more details to the state. Looking good

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good, the bad and the evil'},
-    { title: 'La Necia: Uncensored'},
-    { title: 'Wild Horsie Got A Wooped'},
-    { title: 'Eloquent Nelcia'}
+    { title: 'Javascript: The Good, the bad and the evil', pages: 101},
+    { title: 'La Necia: Uncensored', pages: 39},
+    { title: 'Wild Horsie Got A Wooped', pages: 43},
+    { title: 'Eloquent Nelcia', pages: 23}
   ];
 }
